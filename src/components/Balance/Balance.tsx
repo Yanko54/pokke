4afc32bccbe062776ea0f2cd6701bf1db2e8863a
Yanko <206716253+Yanko54@users.noreply.github.com>
2026-07-12 16:55:40 +0900

    Balanceのレイアウト見直し

--- src/components/Balance/Balance.tsx
+++ src/components/Balance/Balance.tsx
@@ -7,14 +7,16 @@ type BalanceProps = {
 
 export const Balance = ({ amount }: BalanceProps) => {
   return (
-    <div className={styles.balance}>
+    <div className={styles.card}>
       <div className={styles.content}>
         <p className={styles.label}>おさいふ</p>
-        <img className={styles.icon} src={coin} alt="コイン" />
-        <p className={styles.amount}>
-          {amount.toLocaleString()}
-          <span>円</span>
-        </p>
+        <div className={styles.row}>
+          <img className={styles.icon} src={coin} alt="コイン" />
+          <p className={styles.amount}>
+            {amount.toLocaleString()}
+            <span>円</span>
+          </p>
+        </div>
       </div>
     </div>
   );
